48fe6654ec874b0c7c88caaa11bf9c75e0a562b1
swarup260 <swarup260@gmail.com>
2017-11-12 00:51:56 +0530

    save to file added

--- scarping_1.py
+++ scarping_1.py
@@ -22,3 +22,18 @@ def getLink(url):
 data = soup(getLink("https://myanimelist.net/") ,'html.parser')
 print(data.title)
 """
+'''
+BeautifulSoup parser
+1.html.parser
+2.lxml (faster)
+3.lxml-xml
+4.html5lib (Very slow /External Python dependency)
+'''
+##get all the links 
+html_data = getLink("")
+data = soup(html_data,'html.parser')
+with open('links.txt' ,'w') as f:
+    for link in data.find_all('a'):
+        f.write("\n"+str(link.get('href')))
+    f.close()
+            
